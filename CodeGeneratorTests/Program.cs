705b6aadf58d714341545c3028ba7410f3b6022d
Ruan Cardoso <61716126+RuanCardoso@users.noreply.github.com>
2023-12-07 09:01:39 -0400

    Generating virtual methods

--- CodeGeneratorTests/Program.cs
+++ CodeGeneratorTests/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public partial class Test
+{
+	public static void Main()
+	{
+	}
+
+	[Remote]
+	partial void Move(DataIOHandler IOHandler, ushort fromId, ushort toId, RemoteStats stats);
+
+	public override void MoveServerLogic(DataIOHandler IOHandler, ushort fromId, ushort toId, RemoteStats stats)
+	{
+
+	}
+}
+
+public struct RemoteStats
+{
+}
+
+public class DataIOHandler
+{
+}
+
+public class RemoteAttribute : Attribute
+{
+
+}
